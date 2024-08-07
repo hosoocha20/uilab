7ed8ae3a608eb092f839f80916850f692db0be0f
Hosoo Cha <hosoo.cha@gmail.com>
2024-08-07 18:00:50 +1200

    navbar z-index fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <nav className='w-full border-b sticky top-0 h-lg-nav bg-background font-inter  flex items-center gap-[3rem] text-text'>
+    <nav className='w-full border-b sticky top-0 h-lg-nav bg-background font-inter  flex items-center gap-[3rem] text-text z-10'>
         <p className='text-[1.5rem] font-bold tracking-wider   w-[220px] text-center '><span className='text-turquoise-400'>UI</span>Lab</p>
         <div className='font-[500] tracking-wide flex gap-[3rem]   flex-1'>
             <Link to={'/components'}>Components</Link>
