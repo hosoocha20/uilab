4678692d7556024a2aa73694e741583bf0d84ad9
Hosoo Cha <hosoo.cha@gmail.com>
2024-08-07 16:50:57 +1200

    TextReveal Components

--- src/components/TextReveal.tsx
+++ src/components/TextReveal.tsx
@@ -3,7 +3,7 @@ import SyntaxHighlighter from "react-syntax-highlighter"
 import { qtcreatorDark} from "react-syntax-highlighter/dist/esm/styles/hljs";
 import CodeSyntax from "../uiComponents/CodeSyntax";
 import ComponentView from "../uiComponents/ComponentView";
-import { TextRevealUp } from "../uiComponents/TextRevealComponents";
+import { StaggeredTextRevealUp, TextRevealUp, TextSlideUp, TextZoomInUp } from "../uiComponents/TextRevealComponents";
 
 const TextReveal = () => {
   const codeString = 
@@ -72,6 +72,161 @@ export const TextRevealUp = () => {
     </div>
   );
 };'/>
+            <ComponentView component={StaggeredTextRevealUp} name="Staggered Text Reveal Up" header="StaggeredTextRevealUp" codeString='import { motion, Variants } from "framer-motion";
+import React from "react";
+
+export const StaggeredTextRevealUp = ( props: {text: string}) => {
+
+    const variants: Variants = {
+        hidden : {
+            opacity: 0
+        },
+        visible : {
+            opacity: 1, transition: { duration: 0.4, staggerChildren: 0.05, delayChildren: 0.3,ease: "easeOut" }
+        }
+    }
+    const childVariants: Variants = {
+        hidden : {
+            opacity: 0, y: 20,
+        },
+        visible : {
+            opacity: 1, y: 0, transition: { duration: 0.2, ease: "easeOut" }
+        }
+    }
+
+    return(
+        <div className="font-inter h-[300px] border rounded-[10px] flex items-center justify-center bg-black">
+            <motion.div variants={variants} initial={"hidden"} whileInView={"visible"} className="text-white text-[3.5rem] font-bold tracking-wider ">
+                {props.text.split("").map((char : string, i : number) => {
+                    return(
+                        <motion.span key={i} variants={childVariants} className="inline-block">{char}</motion.span>
+                    )
+                })}
+                
+            </motion.div>
+        </div>
+    )
+}'/>
+            <ComponentView component={TextSlideUp} name="Text Slide Up" header="TextSlideUp" codeString='import { motion, Variants } from "framer-motion";
+import React from "react";
+
+export const TextSlideUp = () => {
+  const variants: Variants = {
+    hidden: {
+      opacity: 1,
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5, staggerChildren: 0.13, ease: "linear" },
+    },
+  };
+  const childVariants: Variants = {
+    hidden: {
+      y: "100%",
+    },
+    visible: {
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" },
+    },
+  };
+  return (
+    <div className="font-inter h-[300px] border rounded-[10px] flex flex-col items-center justify-center bg-white">
+      <motion.div
+        variants={variants}
+        initial={"hidden"}
+        whileInView={"visible"}
+      >
+        <div className="text-text text-[3.5rem] font-bold leading-tight relative overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full pl-4"
+          >
+            Text
+          </motion.p>
+        </div>
+        <div className="text-text text-[3.5rem] font-bold leading-tight relative overflow-hidden  ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full"
+          >
+            slide
+          </motion.p>
+        </div>
+        <div className="text-text text-[3.5rem] font-bold leading-tight relative overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full pl-12"
+          >
+            up reveal
+          </motion.p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+'/>
+            <ComponentView component={TextZoomInUp} name="Text Zoom In Up" header="TextZoomInUp" codeString='import { motion, Variants } from "framer-motion";
+import React from "react";
+
+export const TextZoomInUp = () => {
+  const variants: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5, staggerChildren: 0.1, ease: "easeOut" },
+    },
+  };
+  const childVariants: Variants = {
+    hidden: {
+      y: "100%",
+      scaleX: 0.7,
+    },
+    visible: {
+      y: 0,
+      scaleX: 1,
+
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+  };
+  return (
+    <div className="font-inter h-[300px] border rounded-[10px] flex flex-col items-center justify-center bg-white ">
+      <motion.div
+        variants={variants}
+        initial={"hidden"}
+        whileInView={"visible"}
+        className="text-text  text-center text-[3rem] font-bold tracking-tight"
+      >
+        <div className="overflow-hidden  relative ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full leading-snug"
+          >
+            The Art of Design:
+          </motion.p>
+        </div>
+        <div className="overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform h-full leading-snug"
+          >
+            Where Function meets
+          </motion.p>
+        </div>
+        <div className="overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform h-full leading-snug"
+          >
+            Fantasy
+          </motion.p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};'/>
+       
         </div>
     </div>
   );
--- src/uiComponents/TextRevealComponents.tsx
+++ src/uiComponents/TextRevealComponents.tsx
@@ -31,3 +31,214 @@ export const TextRevealUp = () => {
     </div>
   );
 };
+
+export const TextSlideUp = () => {
+  const variants: Variants = {
+    hidden: {
+      opacity: 1,
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5, staggerChildren: 0.13, ease: "linear" },
+    },
+  };
+  const childVariants: Variants = {
+    hidden: {
+      y: "100%",
+    },
+    visible: {
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" },
+    },
+  };
+  return (
+    <div className="font-inter h-[300px] border rounded-[10px] flex flex-col items-center justify-center bg-white">
+      <motion.div
+        variants={variants}
+        initial={"hidden"}
+        whileInView={"visible"}
+      >
+        <div className="text-text text-[3.5rem] font-bold leading-tight relative overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full pl-4"
+          >
+            Text
+          </motion.p>
+        </div>
+        <div className="text-text text-[3.5rem] font-bold leading-tight relative overflow-hidden  ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full"
+          >
+            slide
+          </motion.p>
+        </div>
+        <div className="text-text text-[3.5rem] font-bold leading-tight relative overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full pl-12"
+          >
+            up reveal
+          </motion.p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export const TextZoomInUp = () => {
+  const variants: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5, staggerChildren: 0.1, ease: "easeOut" },
+    },
+  };
+  const childVariants: Variants = {
+    hidden: {
+      y: "100%",
+      scaleX: 0.7,
+    },
+    visible: {
+      y: 0,
+      scaleX: 1,
+
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+  };
+  return (
+    <div className="font-inter h-[300px] border rounded-[10px] flex flex-col items-center justify-center bg-white ">
+      <motion.div
+        variants={variants}
+        initial={"hidden"}
+        whileInView={"visible"}
+        className="text-text  text-center text-[3rem] font-bold tracking-tight"
+      >
+        <div className="overflow-hidden  relative ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform   h-full leading-snug"
+          >
+            The Art of Design:
+          </motion.p>
+        </div>
+        <div className="overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform h-full leading-snug"
+          >
+            Where Function meets
+          </motion.p>
+        </div>
+        <div className="overflow-hidden ">
+          <motion.p
+            variants={childVariants}
+            className="will-change-transform h-full leading-snug"
+          >
+            Fantasy
+          </motion.p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export const StaggeredTextRevealUp = () => {
+  const variants: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+        staggerChildren: 0.05,
+        delayChildren: 0.3,
+        ease: "easeOut",
+      },
+    },
+  };
+  const childVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.2, ease: "easeOut" },
+    },
+  };
+
+  const text = "Staggered";
+  const text1 = "Text";
+  const text2 = "Animation";
+
+  return (
+    <div className="font-inter h-[300px] border rounded-[10px] flex flex-col items-center  bg-black overflow-y-auto">
+      <div className="min-h-[300px] flex items-center ">
+        <motion.div
+          variants={variants}
+          initial={"hidden"}
+          whileInView={"visible"}
+          className="text-white text-[3.5rem] font-bold tracking-wider"
+        >
+          {text.split("").map((char: string, i: number) => {
+            return (
+              <motion.span
+                key={i}
+                variants={childVariants}
+                className="inline-block"
+              >
+                {char}
+              </motion.span>
+            );
+          })}
+        </motion.div>
+      </div>
+      <div className="min-h-[300px] flex items-center ">
+        <motion.div
+          variants={variants}
+          initial={"hidden"}
+          whileInView={"visible"}
+          className="text-white text-[3.5rem] font-bold tracking-wider "
+        >
+          {text1.split("").map((char: string, i: number) => {
+            return (
+              <motion.span
+                key={i}
+                variants={childVariants}
+                className="inline-block"
+              >
+                {char}
+              </motion.span>
+            );
+          })}
+        </motion.div>
+      </div>
+      <div className="min-h-[300px] flex items-center">
+        <motion.div
+          variants={variants}
+          initial={"hidden"}
+          whileInView={"visible"}
+          className="text-white text-[3.5rem] font-bold tracking-wider "
+        >
+          {text2.split("").map((char: string, i: number) => {
+            return (
+              <motion.span
+                key={i}
+                variants={childVariants}
+                className="inline-block"
+              >
+                {char}
+              </motion.span>
+            );
+          })}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
